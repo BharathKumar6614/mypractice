d8a85521ddadeb67287e98a7d455b9ecb882f0c6
BharathKumar6614 <Bharathkumar6614@gmail.com>
2024-09-20 14:17:29 +0530

    nn

--- src/main/java/file1/file2.java
+++ src/main/java/file1/file2.java
@@ -4,16 +4,12 @@ public class file2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
 System.out.println("hello");
 	System.out.println("welcome");	
-=======
+
 System.out.println("final");
->>>>>>> Stashed changes
-=======
-System.out.println("final");
->>>>>>> Stashed changes
+
 	}
 
 }
